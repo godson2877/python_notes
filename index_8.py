15ed973e8eaa0f05bbbe363e55237d94803a6f54
Godson Joseph <godsonjoseph29-@gmail.com>
2026-08-10 10:04:32 +0530

    import functions

---
+++ index_8.py
@@ -0,0 +1,13 @@
+# import math 
+# print(math.sqrt(16))
+
+# from math import sqrt
+# print(sqrt(16))
+# output 4.0
+
+# from math import sqrt,pow
+# print(sqrt(16))
+# print(pow(2,3))
+# output:
+# 4.0
+# 8.0
