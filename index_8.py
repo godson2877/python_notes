0de678b6b37973ae9ff15ecbfae9f94bde6c0df1
Godson Joseph <godsonjoseph29-@gmail.com>
2026-08-10 10:19:15 +0530

    modules and packages

--- index_8.py
+++ index_8.py
@@ -11,3 +11,15 @@
 # output:
 # 4.0
 # 8.0
+
+
+# import math as m
+# print(m.sqrt(16))
+# 
+
+# from datetime import datetime as dt
+# print(dt.now())
+
+import calculator
+print(calculator.add(5, 3))  # Output: 8
+
