2ee6fbbafb34477eb1c7e3e8d8da9530d3227d5a
Godson Joseph <godsonjoseph29-@gmail.com>
2026-08-13 11:06:55 +0530

    file handling 13-08-2026

---
+++ file_handling.py
@@ -0,0 +1,57 @@
+# f=open("sample.txt","w")
+# f.write("learn python")
+# f.close
+
+
+
+# f=open(r"C:\Users\godso\phython\data.txt","w")
+# f.close
+
+# f=open("sample.txt","r")
+# print(f.read())
+# f.close
+
+# f=open("sample.txt","r")
+# # f.write(" learn javascript\n")
+# # print(f.readline())
+# # print(f.readline())
+# # print(f.readline())
+# print(f.readlines())
+
+# f.close
+
+
+
+# f=open("sample_1.txt","x")
+# f.close()
+
+# f=open("Screenshot 2026-07-26 091614.png","rb")
+# # print(f.read())
+# print(f.read(20))
+# f.close
+
+# r+
+# f=open("sample.txt","r+")
+# print(f.read())
+# f.write("goodbye\n")
+# print(f.read())
+# f.close
+# f=open("sample.txt","r+")
+# print(f.read())
+# f.close
+
+
+# w+
+# f=open("sample.txt","w+")
+# print(f.read())
+# f.write("goodbye\n")
+# print(f.read())
+# f.close
+
+
+
+f=open("sample_3.txt","a+")
+print(f.read())
+f.write("goodbye\n")
+print(f.read())
+f.close
