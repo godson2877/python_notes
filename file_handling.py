0fd969d2660bc214713486c4d4ed7e02bbeb2b21
Godson Joseph <godsonjoseph29-@gmail.com>
2026-08-13 11:21:30 +0530

    file handling 13-08-2026

--- file_handling.py
+++ file_handling.py
@@ -1,15 +1,15 @@
 # f=open("sample.txt","w")
 # f.write("learn python")
-# f.close
+# f.close()
 
 
 
 # f=open(r"C:\Users\godso\phython\data.txt","w")
-# f.close
+# f.close()
 
 # f=open("sample.txt","r")
 # print(f.read())
-# f.close
+# f.close()
 
 # f=open("sample.txt","r")
 # # f.write(" learn javascript\n")
@@ -18,7 +18,7 @@
 # # print(f.readline())
 # print(f.readlines())
 
-# f.close
+# f.close()
 
 
 
@@ -28,17 +28,17 @@
 # f=open("Screenshot 2026-07-26 091614.png","rb")
 # # print(f.read())
 # print(f.read(20))
-# f.close
+# f.close()
 
 # r+
 # f=open("sample.txt","r+")
 # print(f.read())
 # f.write("goodbye\n")
 # print(f.read())
-# f.close
+# f.close()
 # f=open("sample.txt","r+")
 # print(f.read())
-# f.close
+# f.close()
 
 
 # w+
@@ -46,12 +46,37 @@
 # print(f.read())
 # f.write("goodbye\n")
 # print(f.read())
+# f.close()
+
+
+# a+
+# f=open("sample_3.txt","a+")
+# print(f.read())
+# f.write("goodbye\n")
+# print(f.read())
+# f.close()
+
+
+
+# f=open("sample_4.txt","w")
+# f.write("welcome to python")
+# print(f.name)
+# print(f.mode)
+# print(f.closed)
+# f.close()
+# print(f.closed)\
+
+
+
+
+# f=open("sample.txt","w")
+# f.writelines(['hello world\n','learn python\n', 'learn java\n', 'learn c \n '])
 # f.close
 
 
 
-f=open("sample_3.txt","a+")
-print(f.read())
-f.write("goodbye\n")
-print(f.read())
-f.close
+f=open("sample.txt","w")
+f.write("learn python")
+f.close()
+with open("data.txt","w") as f:
+    f.write("le")
