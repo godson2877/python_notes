16fb25f80991c1d460b5a911d75fbc884e657d86
Godson Joseph <godsonjoseph29-@gmail.com>
2026-08-31 07:40:15 +0530

    object creation

---
+++ index_14.py
@@ -0,0 +1,76 @@
+# class dogs:
+#     # name="karan"
+#     def __init__(self,name):
+#         self.name=name
+#     def bark(self):
+#         print(f"{self.name} says woof !")
+# d=dogs("rex")
+# print(d.name)
+# d.bark()
+# d=dogs("karan")
+# d.bark()
+# d1=dogs("varun")
+# d1.bark()
+
+
+# 1)
+
+# class student:
+#     def study(self):
+#         print("student is studying")
+# s=student()
+# s.study()
+
+
+# class car:
+#     def drive(self):
+#         print("car is driving")
+# c=car()
+# c.drive()
+
+# class student:
+#     name="rahul"
+#     def study(self):
+#         print(f"{self.name} is studying")
+# s=student()
+# s.study()
+
+
+# class dog:
+#     name ="rex"
+#     def bark(self):
+#         print(f"{self.name} is barking")
+# d=dog()
+# d.bark()
+
+
+
+# class student:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+    
+# s=student("anu",10)
+# print(s.name)
+# print(s.age)
+
+
+# class car:
+#     def __init__(self,brand,model):
+#         self.brand=brand
+#         self.model=model
+#     def display(self):
+#         print(f"{self.brand},{self.model}")
+# c=car("toyota","inova")
+# c.display()
+
+
+class student:
+    def __init__(self,name):
+        self.name=name
+    def introduce(self):
+        print(f"my name is {self.name}")
+s=student("rahul")
+s.introduce()
+s=student("anu")
+s.introduce()
