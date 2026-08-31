65d0f0054c07af308d52ad668482303f4e1c5e94
Godson Joseph <godsonjoseph29-@gmail.com>
2026-08-31 08:50:42 +0530

    acess modifier

---
+++ index_!5.py
@@ -0,0 +1,22 @@
+# class student:
+#     name="rex"
+# s=student()
+# print(s.name)
+
+
+
+# class example:
+#     __private =3
+# e=example()
+
+# # print(e.__private)
+# print(e._example__private)
+
+# class account:
+#     _balance=1000
+# class savingsaccount(account):
+#     def show_balance(self):
+#         print(self._balance)
+# acc = savingsaccount()
+# acc.show_balance()
+# print(acc._balance)
