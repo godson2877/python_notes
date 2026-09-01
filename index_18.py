bc73a46409d9610043840cf3e96fd8fd3129ff56
Godson Joseph <godsonjoseph29-@gmail.com>
2026-09-01 09:22:11 +0530

    oops

---
+++ index_18.py
@@ -0,0 +1,40 @@
+# abstraction
+# from abc import ABC,abstractmethod
+
+# class Shape(ABC):
+#     @abstractmethod
+#     def area(self):
+#         pass
+# class square(Shape):
+#     def area(self):
+#         return 4*4
+# class rectangle(Shape):
+#     def area(self):
+#         return 5*4
+
+# # d=square()
+# # print(d.area())
+# # d=rectangle()
+# # print(d.area())
+# d=Shape()
+# print(d.area())
+
+
+
+# class demo :
+#     def show(self):
+#         print("instance method")
+# s=demo()
+# s.show()
+
+# class demo:
+#     @classmethod
+#     def info(cls):
+#         print("classmethod")
+# demo.info()
+
+class demo:
+    @staticmethod
+    def utility():
+        print("static method")
+demo.utility()
